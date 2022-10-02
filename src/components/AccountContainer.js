f0d167b57bfadfcc0582d994d0c371fbebf3302b
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-02 13:00:22 +0300

    fetch data from json server

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,8 +2,17 @@ import React from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
+import {useSate} from "react";
 
 function AccountContainer() {
+  // const [transaction, setTransaction] = useSate([])
+  function fetchTransactions(){
+    fetch("http://localhost:8001/transactions")
+    .then((response) => response.json())
+    .then((data)=> console.log(data))
+  }
+
+  fetchTransactions()
   return (
     <div>
       <Search />
