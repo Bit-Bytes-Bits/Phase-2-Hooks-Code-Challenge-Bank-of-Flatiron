dde034779296054327094f60f42587be5d7e00ba
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-02 15:03:24 +0300

    map over the object and return to the transaction component and display data in the table

--- src/components/TransactionsList.js
+++ src/components/TransactionsList.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Transaction from "./Transaction";
 
-function TransactionsList() {
+function TransactionsList({transaction}) {
   return (
     <table className="ui celled striped padded table">
       <tbody>
@@ -20,6 +20,8 @@ function TransactionsList() {
           </th>
         </tr>
         {/* render a list of <Transaction> components here */}
+        {transaction.map(transactionObject => { return <Transaction key={transactionObject.id} transactionObject={transactionObject}/>
+        })}
       </tbody>
     </table>
   );
