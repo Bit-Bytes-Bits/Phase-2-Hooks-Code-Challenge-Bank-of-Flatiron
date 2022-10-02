2c66af7b859881102972986377741e0d904d84b2
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-02 15:00:03 +0300

    add an object to store all transactions and display on table

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,12 +1,13 @@
 import React from "react";
 
-function Transaction() {
+function Transaction({transactionObject}) {
+  const {id, date, description, category, amount}= transactionObject;
   return (
     <tr>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
-      <td>{"your code here..."}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
     </tr>
   );
 }
