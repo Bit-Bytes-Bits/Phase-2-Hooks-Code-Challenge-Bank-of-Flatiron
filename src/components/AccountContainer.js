3cc351181d829149311ac86f3a71980d9a608bba
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-04 11:39:00 +0300

    modify fetch function to retrive and post new data and add useStates

--- src/components/AccountContainer.js
+++ src/components/AccountContainer.js
@@ -2,27 +2,39 @@ import React, { useEffect } from "react";
 import TransactionsList from "./TransactionsList";
 import Search from "./Search";
 import AddTransactionForm from "./AddTransactionForm";
-import {useState} from "react";
+import { useState } from "react";
 
 function AccountContainer() {
-  const [transaction, setTransaction] = useState([])
-  function fetchTransactions(){
+  const [transaction, setTransaction] = useState([]);
+  const [searchString, setSearch] = useState("");
+  function fetchTransactions() {
     fetch("http://localhost:8001/transactions")
-    .then((response) => response.json())
-    .then((data)=> {
-      setTransaction(data)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTransaction(data);
+      });
   }
 
-useEffect(()=> {
-  fetchTransactions()
-}, [])
+  useEffect(() => {
+    fetchTransactions();
+  }, []);
+
+  function handleFormSubmit(dataObject) {
+    fetch("http://localhost:8001/transactions", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        ...dataObject,
+        "amount": parseInt(dataObject.amount)
+      })
+    }).then(() => fetchTransactions());
+  }
 
   return (
     <div>
-      <Search />
-      <AddTransactionForm />
-      <TransactionsList transaction={transaction}/>
+      <Search setSearch={setSearch} />
+      <AddTransactionForm submit={handleFormSubmit} />
+      <TransactionsList  searchString={searchString} transaction={transaction} />
     </div>
   );
 }
