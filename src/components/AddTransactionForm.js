f01afb72c893ad5680985a669f83efd3ddd9cf0f
Ian Mike <ian.kaibi@student.moringaschool.com>
2022-10-04 11:34:57 +0300

    add state to when user adds a new transaction and respective event listeners

--- src/components/AddTransactionForm.js
+++ src/components/AddTransactionForm.js
@@ -1,16 +1,32 @@
 import React from "react";
+import {useState} from "react";
 
-function AddTransactionForm() {
+function AddTransactionForm({submit}) {
+
+  const [addTransaction, setAddTransaction] = useState({
+    date:"",
+    category:"",
+    amount:"null",
+    description:""
+  })
+  function handleFormData(event){
+    setAddTransaction({...addTransaction, [event.target.name]:event.target.value})
+  }
+
+  function submitData(event){
+    event.preventDefault()
+    submit(addTransaction)
+  }
   return (
     <div className="ui segment">
       <form className="ui form">
         <div className="inline fields">
-          <input type="date" name="date" />
-          <input type="text" name="description" placeholder="Description" />
-          <input type="text" name="category" placeholder="Category" />
-          <input type="number" name="amount" placeholder="Amount" step="0.01" />
+          <input type="date" name="date" onBlur={handleFormData} />
+          <input type="text" name="description" placeholder="Description" onBlur={handleFormData} />
+          <input type="text" name="category" placeholder="Category" onBlur={handleFormData} />
+          <input type="number" name="amount" placeholder="Amount" step="0.01" onBlur={handleFormData} />
         </div>
-        <button className="ui button" type="submit">
+        <button className="ui button" type="submit" onClick={submitData}>
           Add Transaction
         </button>
       </form>
